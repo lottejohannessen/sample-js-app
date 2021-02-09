548487fe0eb96e47b9b73a634f40e257300059c5
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-02-09 09:44:05 -0800

    identify pokemon variables and info

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass","poison"]},
+    {name: "Snorlax", height: 2.1, weiht: 460, types:["normal"]},
+    {name: "squirtle", height: 0.5, weight: 8.5, types:["fire"]}
+  ];
+    }
+})
