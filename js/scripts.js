971fcb1a859fe5614cee7cebcd5506d6b10459da
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-02-10 15:45:55 -0800

    variables
    cleaned up code and added pokemon variable and discriptions

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let pokemonList = {
+  bulbasaur: {
+    name:"Bulbasaur", height:0.7, types:"grass","poison"
+  };
+  ivysaur: {
+    name: "Ivysaur", height:1, types:"grass","poison"
+  };
+  charmander: {
+    name: "Charmander", height:0.6, types:"fire"
+  };
+}
