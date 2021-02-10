be009875748820d059b6f181676eaec45174e823
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-02-10 15:56:38 -0800

    clean up pokemon variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 let pokemonList = {
-  bulbasaur: {
-    name:"Bulbasaur", height:0.7, types:"grass","poison"
-  };
-  ivysaur: {
-    name: "Ivysaur", height:1, types:"grass","poison"
-  };
-  charmander: {
-    name: "Charmander", height:0.6, types:"fire"
-  };
-}
+  {name:"Bulbasaur", height:0.7, types:["grass","poison"]};
+  {name: "Ivysaur", height:1, types:["grass","poison"]};
+  {name: "Charmander", height:0.6, types:["fire"]};
+};
+document.write(Charmander);
+document.write(Ivysaur);
+document.write(Bulbasaur);
