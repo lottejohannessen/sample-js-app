1720339a99d590ba38aacd2cdc774751c4cff53e
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-02-11 11:49:58 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var pokemonList = [
+let pokemonList = [
   {name:"Bulbasaur", height:0.7, types:["grass","poison"]},
   {name: "Ivysaur", height:1, types:["grass","poison"]},
-  {name: "Charmander", height:0.6, types:["fire"]},
+  {name: "Charmander", height:0.6, types:"fire"},
 ];
