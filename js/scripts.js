14bc1790a324a39681546939f8f9a8295e75d7c1
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-02-22 11:17:14 -0800

    code tweaks. fixing errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,60 @@
-let pokemonList = [
-  {
-    name:"Bulbasaur",
-    height:0.7,
-    types:["grass","poison"]
-  },
-  {
-    name: "Ivysaur",
-    height:1,
-    types:["grass","poison"]
-  },
-  {
-    name: "Charmander",
-    height:0.6,
-    types:"fire"
-  },
-];
-let pokemonRepository = (function () {
-  return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass ','poison']
     },
-    getAll: function() {
-      return pokemonList;
+    {
+      name: 'Ivysaur',
+      height: 1,
+      types: ['grass ','poison']
+    },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: 'fire'
+    },
+  ];
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && typeof pokemon !== null) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('you need an object');
     }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-List');
+    let listItem = document.createElement('div');
+    let button = document.createElement('button');
+    button.innerText = ['name: ' + pokemon.name, ' height: ' + pokemon.height, ' types: ' + pokemon.types];
+    button.classList.add('pokemon-list-style');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+  };
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-pokemonRepository.add(
-  {
-    name: "Butterfree",
-    height: 1.1,
-    types: ["bug","flying"]
-   }
- );
- console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Butterfree',height: 1.1,types: ['bug','flying']});
 
-(function(){
-  pokemonList.forEach (function(pokemon){
-    if (pokemonList[i].height >= 1){
-      document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ") " + " - Wow,that's big!<br> ");
-    }
-    else {
-      document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ") <br>");
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
   });
-})();
