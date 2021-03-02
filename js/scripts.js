1afa632205134bf8ec596918713617c377fe4064
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-03-02 10:23:58 -0800

    assignment 1.7
    adding all pokemon and details

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 let pokemonRepository = (function() {
-  let pokemonList = [
-    {
-      name: 'Bulbasaur',
-      height: 0.7,
-      types: ['grass ','poison']
-    },
-    {
-      name: 'Ivysaur',
-      height: 1,
-      types: ['grass ','poison']
-    },
-    {
-      name: 'Charmander',
-      height: 0.6,
-      types: 'fire'
-    },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (typeof pokemon === 'object' && typeof pokemon !== null) {
@@ -45,6 +30,33 @@ let pokemonRepository = (function() {
       showDetails(pokemon);
     });
   };
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }
+function loadDetails(item){
+  let url= item.detailsUrl;
+  return fetch(url).then(function(response) {
+    return response.json();
+  }).then(funtion(details){
+    item.imageUrl=details.sprites.front_default;
+    item.height=details.height;
+    item.types=details.types;
+  }).catch(function(e){
+    console.error(e);
+  });
+};
 
   return {
     add: add,
@@ -53,8 +65,8 @@ let pokemonRepository = (function() {
   };
 })();
 
-pokemonRepository.add({name: 'Butterfree',height: 1.1,types: ['bug','flying']});
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+      pokemonRepository.addListItem(pokemon);
+    });
+});
